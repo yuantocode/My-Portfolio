17c7507148ccc50183962f39ef21c8d62fe6271e
yuuaaannnn <>
2025-07-13 22:12:02 +0800

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -59,7 +59,7 @@ export const ContactSection = () => {
                     href="mailto:UanJimenez0408@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    UanJimenez0408@gmail.com
+                    UanJimenezzzz0408@gmail.com
                   </a>
                 </div>
               </div>
